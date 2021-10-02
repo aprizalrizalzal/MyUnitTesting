fe634ba0edd02848652422c2a103728c4db949a8
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2021-10-02 22:37:39 +0800

    Initial commit

---
+++ app/src/main/java/me/aprizal/myunittesting/CuboidModel.java
@@ -0,0 +1,30 @@
+package me.aprizal.myunittesting;
+
+public class CuboidModel {
+    private double length;
+    private double width;
+    private double height;
+
+    public CuboidModel() {
+    }
+
+    public void save(double length,double width, double height){
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    double getVolume() {
+        return length*width*height;
+    }
+
+    public double getSurfaceArea() {
+        double lw = length * width;
+        double wh = width * height;
+        double lh = length * height;
+        return 2 * (lw + wh + lh);
+    }
+    public double getCircumference() {
+        return 4 * (length + width + height);
+    }
+}
